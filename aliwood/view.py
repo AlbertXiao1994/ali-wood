11c0651c622f9887491cff756a01dee1fb6ea14a
Albert <albert_zju@163.com>
2019-03-18 10:40:10 +0800

    componentify

--- aliwood/view.py
+++ aliwood/view.py
@@ -3,24 +3,24 @@ from django.shortcuts import render
 def home(request):
   context          = {}
   context['path'] = request.path
-  return render(request, 'aliwood/home.html', context)
+  return render(request, 'aliwood/pages/home.html', context)
 
 def data(request):
   context          = {}
   context['path'] = request.path
-  return render(request, 'aliwood/data.html', context)
+  return render(request, 'aliwood/pages/data.html', context)
 
 def edit(request):
   context          = {}
   context['path'] = request.path
-  return render(request, 'aliwood/edit.html', context)
+  return render(request, 'aliwood/pages/edit.html', context)
 
 def rank(request):
   context          = {}
   context['path'] = request.path
-  return render(request, 'aliwood/rank.html', context)
+  return render(request, 'aliwood/pages/rank.html', context)
 
 def history(request):
   context          = {}
   context['path'] = request.path
-  return render(request, 'aliwood/history.html', context)
+  return render(request, 'aliwood/pages/history.html', context)
