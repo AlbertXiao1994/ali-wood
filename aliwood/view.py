0e4a617ddeceefc554c302a7462a6a154c601576
Albert <albert_zju@163.com>
2019-03-24 01:03:01 +0800

    feat: add video-data page

--- aliwood/view.py
+++ aliwood/view.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
  
 def home(request):
   context          = {}
---
+++ static/js/player-shadow.js
@@ -0,0 +1,19 @@
+// 打开视频遮罩
+$('.video-item').click(function () {
+  if ($("#helper-wrapper").css('display') === 'block') {
+    $('.player-shadow').css('right', '4.43rem');
+  } else {
+    $('.player-shadow').css('right', '0');
+  }
+  $("#hepler-icon-open").hide();
+  $('.player-shadow').show();
+  $('.player-shadow').css('display', 'flex');
+});
+
+// 关闭视频遮罩
+$('#player-btn-close').click(function () {
+  if ($("#helper-wrapper").css('display') === 'none') {
+    $("#hepler-icon-open").show();
+  }
+  $('.player-shadow').hide();
+});
