5b19b5231a3a27eaee753f7ff9a271ac1d320a62
Albert <albert_zju@163.com>
2019-04-09 01:19:19 +0800

    update

--- static/js/player-shadow.js
+++ static/js/player-shadow.js
@@ -1,21 +1,31 @@
-// 打开视频遮罩
-$('.video-item').click(function () {
-  if ($("#helper-wrapper").css('display') === 'block') {
-    $('.player-shadow').css('right', '4.43rem');
-  } else {
-    $('.player-shadow').css('right', '0');
-  }
-  $("#hepler-icon-open").hide();
-  $('.player-shadow').show();
-  $('.player-shadow').css('display', 'flex');
-  $(this).siblings().removeClass('active');
-  $(this).addClass('active');
-});
+initDplayer();
+function initDplayer() {
+  // 加载dplayer
+  var dp = new DPlayer({
+    container: document.getElementById('dplayer')
+  });
 
-// 关闭视频遮罩
-$('#player-btn-close').click(function () {
-  if ($("#helper-wrapper").css('display') === 'none') {
-    $("#hepler-icon-open").show();
-  }
-  $('.player-shadow').hide();
-});
+  // 打开视频遮罩
+  $('.video-item').click(function () {
+    var newUrl = $(this).attr('src');
+    dp.switchVideo({url: newUrl});
+    if ($("#helper-wrapper").css('display') === 'block') {
+      $('.player-shadow').css('right', '4.43rem');
+    } else {
+      $('.player-shadow').css('right', '0');
+    }
+    $("#hepler-icon-open").hide();
+    $('.player-shadow').show();
+    $('.player-shadow').css('display', 'flex');
+    $(this).siblings().removeClass('active');
+    $(this).addClass('active');
+  });
+
+  // 关闭视频遮罩
+  $('#player-btn-close').click(function () {
+    if ($("#helper-wrapper").css('display') === 'none') {
+      $("#hepler-icon-open").show();
+    }
+    $('.player-shadow').hide();
+  });
+}
